2e9c5780c1efdde7847e0fb3665f6b1655483920
Kerim Mandzo <kerman07@live.com>
2020-11-01 18:47:13 +0100

    changed all urls

--- network/urls.py
+++ network/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("new_post", views.new_post, name="new_post"),
-    path("following", views.following, name="following"),
-    path("follow", views.follow, name="follow"),
-    path("edit", views.edit, name="edit"),
-    path("like", views.like, name="like"),
-    path("profile/<int:id>", views.profile, name="profile")
+    path("/", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("new_post/", views.new_post, name="new_post"),
+    path("following/", views.following, name="following"),
+    path("follow/", views.follow, name="follow"),
+    path("edit/", views.edit, name="edit"),
+    path("like/", views.like, name="like"),
+    path("profile/<int:id>/", views.profile, name="profile")
 ]
